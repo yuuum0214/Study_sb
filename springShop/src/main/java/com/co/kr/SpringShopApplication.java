a6d55632cbf2ea76dbabb00772265c15bdabdd94
yuuum0214 <cdcd0214@gmail.com>
2023-05-06 21:07:01 +0900

    First Commit

--- springShop/src/main/java/com/co/kr/SpringShopApplication.java
+++ springShop/src/main/java/com/co/kr/SpringShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringEx1Application {
+public class SpringShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEx1Application.class, args);
+		SpringApplication.run(SpringShopApplication.class, args);
 	}
 
 }
