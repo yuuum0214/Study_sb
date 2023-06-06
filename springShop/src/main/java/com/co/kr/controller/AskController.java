5398836429ea7e7fe73b5a61eb9f86bd080f7fff
yuuum0214 <cdcd0214@gmail.com>
2023-06-06 16:37:12 +0900

    0606_commit(1)

--- springShop/src/main/java/com/co/kr/controller/AskController.java
+++ springShop/src/main/java/com/co/kr/controller/AskController.java
@@ -1,15 +1,22 @@
 package com.co.kr.controller;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.co.kr.domain.AskListDomain;
 import com.co.kr.service.AskService;
+import com.co.kr.vo.AskFileListVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -45,13 +52,35 @@ public class AskController {
 	public String openAskEdit() throws Exception{
 		return "ask/askEdit";
 	}
+	
+	
 	//문의사항 작성 후 연동
 	@RequestMapping("/ask/insertAsk")
 	public String insertAsk(AskListDomain askListDomain) throws Exception{
 		askService.insertAsk(askListDomain);
 		return "redirect:/ask";
 	}
+	/*
+	//문의사항에 파일 첨부해서 새로 연동
+	@PostMapping(value = "/ask/insertAsk")
+	public ModelAndView insertAsk(AskFileListVO askFileListVO, MultipartHttpServletRequest request,
+								HttpServletRequest httpRequest) throws IOException, ParseException{
+		ModelAndView mav = new ModelAndView();
+		int ibSeq = askService.fileProcess(askFileListVO, request, httpRequest);
+		askFileListVO.setContent("");
+		askFileListVO.setTitle("");
+		
+		mav = askListSelect(askFileListVO, String.valueOf(ibSeq), request);
+		mav.setViewName("ask.html");
+		return mav;
+	}
 	
+	private ModelAndView askListSelect(AskFileListVO askFileListVO, String valueOf,
+			MultipartHttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+
 	//문의사항 읽어오기
 	@RequestMapping("/ask/openAskDetail")
 	public ModelAndView openAskDetail(@RequestParam int ibSeq) throws Exception{
--- springShop/src/main/java/com/co/kr/service/AskServiceImpl.java
+++ springShop/src/main/java/com/co/kr/service/AskServiceImpl.java
@@ -1,12 +1,31 @@
 package com.co.kr.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.co.kr.code.Code;
+import com.co.kr.domain.AskFileDomain;
 import com.co.kr.domain.AskListDomain;
+import com.co.kr.exception.RequestException;
 import com.co.kr.mapper.AskMapper;
+import com.co.kr.util.CommonUtils;
+import com.co.kr.vo.AskFileListVO;
 
 @Service
 public class AskServiceImpl implements AskService {
@@ -43,6 +62,117 @@ public class AskServiceImpl implements AskService {
 	@Override
 	public void deleteAsk(int ibSeq) throws Exception{
 		askMapper.deleteAsk(ibSeq);
+	}
+
+	@Override
+	public int fileProcess(AskFileListVO askFileListVO, MultipartHttpServletRequest request,
+			HttpServletRequest httpReq) {
+		HttpSession session = httpReq.getSession();
+		
+		AskListDomain askListDomain = AskListDomain.builder()
+				.smbId(session.getAttribute("id").toString())
+				.ibTitle(askFileListVO.getTitle())
+				.ibContent(askFileListVO.getContent())
+				.build();
+		
+		int ibSeq = askListDomain.getIbSeq();
+		String smbId = askListDomain.getSmbId();
+		
+		List<MultipartFile> multipartFiles = request.getFiles("file");
+		
+		if(askFileListVO.getIsEdit() != null) {
+			List<AskFileDomain> fileList = null;
+			
+			for(MultipartFile multipartFile : multipartFiles) {
+				if(!multipartFile.isEmpty()) {
+					if(session.getAttribute("file") != null) {
+						fileList = (List<AskFileDomain>) session.getAttribute("file");
+						
+						for(AskFileDomain list : fileList) {
+							list.getIbFilePath();
+							Path filePath = Paths.get(list.getIbFilePath());
+							
+							try {
+								Files.deleteIfExists(filePath);
+								
+							} catch (DirectoryNotEmptyException e) {
+								throw RequestException.fire(Code.E404, "디렉토리가 존재하지 않습니다", HttpStatus.NOT_FOUND);
+					        } catch (IOException e) {
+					            e.printStackTrace();
+					        }
+						}
+					}
+				}
+			}
+		}
+		
+		//새로운 파일 저장 : root 경로 만들기
+		Path rootPath = Paths.get(new File("E://").toString(), "Upload", File.separator).toAbsolutePath().normalize();
+		File pathCheck = new File(rootPath.toString());
+		
+		if(!pathCheck.exists()) pathCheck.mkdirs();
+		
+		for(MultipartFile multipartFile : multipartFiles) {
+			if(!multipartFile.isEmpty()) {
+				String originalFileExtension;
+				String contentType = multipartFile.getContentType();
+				String origFilename = multipartFile.getOriginalFilename();
+				
+				if(ObjectUtils.isEmpty(contentType)) {
+					break;
+				}else {
+					if(contentType.contains("image/jpeg")) {
+						originalFileExtension = ".jpg";
+					}else if(contentType.contains("image/png")) {
+						originalFileExtension = ".png";
+					}else {
+						break;
+					}
+				}
+				
+				String uuid = UUID.randomUUID().toString();
+				String current = CommonUtils.currentTime();
+				String newFileName = uuid + current + originalFileExtension;
+				
+				Path targetPath = rootPath.resolve(newFileName);
+				
+				File file = new File(targetPath.toString());
+				
+				try {
+					multipartFile.transferTo(file);
+					file.setWritable(true);
+					file.setReadable(true);
+					
+					AskFileDomain askFileDomain = AskFileDomain.builder()
+							.ibSeq(ibSeq)
+							.smbId(smbId)
+							.ibOrigFile(origFilename)
+							.ibNewFile("resources/upload/"+newFileName)
+							.ibFilePath(targetPath.toString())
+							.ibFileSize((int)multipartFile.getSize())
+							.build();
+					
+					askMapper.askFileUpload(askFileDomain);
+					System.out.println("upload seccess");
+				} catch (IOException e) {
+					throw RequestException.fire(Code.E404, "잘못된 업로드 파일입니다.", HttpStatus.NOT_FOUND);
+				}
+			}
+		}
+		
+		return ibSeq;
+		/*
+		AskListDomain askListDomain = AskListDomain.builder()
+				.smbId(session.getAttribute("id").toString())
+				.ibTitle(askFileListVO.getTitle())
+				.ibContent(askFileListVO.getContent())
+				.build();
+		
+				if(askFileListVO.getIsEdit() != null) {
+					askListDomain.setIbSeq(Integer.parseInt(askFileListVO.getSeq()));
+					System.out.println("수정업데이트");
+				}
+				*/
 	}
 	
 }
